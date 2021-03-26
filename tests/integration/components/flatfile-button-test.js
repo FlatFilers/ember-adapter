948f0c4257f2f764d8590df6e09401beeafe1d43
bestdoggo <bestdoggo@pm.me>
2021-03-26 13:13:38 -0500

    Fixed failing tests with stubbing

--- tests/integration/components/flatfile-button-test.js
+++ tests/integration/components/flatfile-button-test.js
@@ -2,9 +2,24 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+// import sinon from 'sinon';
+import Service from '@ember/service';
+
+class flatfileServiceStub extends Service {
+  importer = importerStub;
+}
+class importerStub {
+  constructor() {
+    this.$ready = new Promise((resolve) => resolve());
+  }
+}
 
 module('Integration | Component | flatfile-button', function (hooks) {
   setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.owner.register('service:flatfile', flatfileServiceStub);
+  });
 
   test('it renders a button element with yielded text', async function (assert) {
     await render(hbs`
