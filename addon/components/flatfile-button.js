b0b37042f359b702ecc052b5543bb6ab54c9036b
bestdoggo <bestdoggo@pm.me>
2021-03-26 11:38:09 -0500

    Loading and ready states

--- addon/components/flatfile-button.js
+++ addon/components/flatfile-button.js
@@ -1,14 +1,20 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import FlatfileImporter from '@flatfile/adapter';
+import { tracked } from '@glimmer/tracking';
 
 export default class extends Component {
+  @tracked isLoading = this.args.preload !== false;
+  @tracked isReady = false;
+
   constructor() {
     super(...arguments);
     if (this.args.preload !== false) this.initializeFlatfileImporter();
   }
 
   initializeFlatfileImporter() {
+    this.isLoading = true;
+
     if (this.args.mountUrl) FlatfileImporter.setMountUrl(this.args.mountUrl);
 
     this.flatfileImporter = new FlatfileImporter(
@@ -16,6 +22,10 @@ export default class extends Component {
       this.args.settings,
       this.args.customer
     );
+    this.flatfileImporter.$ready.then(() => {
+      this.isReady = true;
+      this.isLoading = false;
+    });
 
     // TODO register callbacks
   }
