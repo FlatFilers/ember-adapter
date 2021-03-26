5a5530c56f2c6e438bdfad61838c476311559f0d
bestdoggo <bestdoggo@pm.me>
2021-03-26 11:18:12 -0500

    Button opens importer, preloading is in

--- addon/components/flatfile-button.js
+++ addon/components/flatfile-button.js
@@ -1,13 +1,28 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import FlatfileImporter from '@flatfile/adapter';
 
 export default class extends Component {
   constructor() {
     super(...arguments);
+    if (this.args.preload !== false) this.initializeFlatfileImporter();
+  }
+
+  initializeFlatfileImporter() {
+    if (this.args.mountUrl) FlatfileImporter.setMountUrl(this.args.mountUrl);
+
     this.flatfileImporter = new FlatfileImporter(
       this.args.licenseKey,
       this.args.settings,
       this.args.customer
     );
+
+    // TODO register callbacks
+  }
+
+  @action
+  loadImporter() {
+    if (!this.flatfileImporter) this.initializeFlatfileImporter();
+    this.flatfileImporter.open();
   }
 }
