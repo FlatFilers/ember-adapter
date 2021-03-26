7a5d5f515e4e1803e0e1355bb3c98543a1067938
bestdoggo <bestdoggo@pm.me>
2021-03-26 10:57:42 -0500

    Flatfile instance set up in constructor

--- addon/components/flatfile-button.js
+++ addon/components/flatfile-button.js
@@ -1,4 +1,13 @@
 import Component from '@glimmer/component';
+import FlatfileImporter from '@flatfile/adapter';
 
 export default class extends Component {
+  constructor() {
+    super(...arguments);
+    this.flatfileImporter = new FlatfileImporter(
+      this.args.licenseKey,
+      this.args.settings,
+      this.args.customer
+    );
+  }
 }
